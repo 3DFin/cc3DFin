c1848e5e511cfb95a1bd0ab15089f0030e117ab1
Romain Janvier <romain.janvier@hotmail.fr>
2025-07-25 15:21:33 +0200

    Import connected components.

---
+++ lib3DFin/include/connected_components.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright 2023-2025 Carlos Cabo <carloscabo@uniovi.es>
+
+#include "types.hpp"
+
+namespace lib3dfin
+{
+	template <typename real_t>
+	VecIndex<int32_t> connected_components(const PointCloud<real_t> xyz, const real_t eps, const uint32_t min_samples);
+} // namespace lib3dfin
